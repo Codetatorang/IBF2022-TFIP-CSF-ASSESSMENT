f824ab6abc97eaf74a05e1814a34f5ee400b3530
Codetatorang <102163869+Codetatorang@users.noreply.github.com>
2023-05-12 11:43:13 +0800

    completed task 1

--- archive_backend/src/main/java/ibf2022/batch2/csf/backend/controllers/UploadController.java
+++ archive_backend/src/main/java/ibf2022/batch2/csf/backend/controllers/UploadController.java
@@ -1,8 +1,31 @@
 package ibf2022.batch2.csf.backend.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.multipart.MultipartFile;
+
+
+@Controller
 public class UploadController {
+	private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
+
 
 	// TODO: Task 2, Task 3, Task 4
+	@PostMapping(path="/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<String> upload(
+		@RequestPart MultipartFile zipfile,
+		@RequestPart String name,
+		@RequestPart String title,
+		@RequestPart String comments
+	) {
+		logger.info("springboot backend called");
+		return null;
+	}
 	
 
 	// TODO: Task 5

5ce2a617be37a3642d6c442f3fd467b2017fc155
Codetatorang <102163869+Codetatorang@users.noreply.github.com>
2023-05-12 16:38:05 +0800

    cannot complete

--- archive_backend/src/main/java/ibf2022/batch2/csf/backend/controllers/UploadController.java
+++ archive_backend/src/main/java/ibf2022/batch2/csf/backend/controllers/UploadController.java
@@ -1,7 +1,18 @@
 package ibf2022.batch2.csf.backend.controllers;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipInputStream;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -9,30 +20,73 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.multipart.MultipartFile;
 
+import ibf2022.batch2.csf.backend.repositories.ImageRepository;
 
 @Controller
 public class UploadController {
+	@Autowired
+	ImageRepository imgRepo;
+
 	private static final Logger logger = LoggerFactory.getLogger(UploadController.class);
 
-
 	// TODO: Task 2, Task 3, Task 4
-	@PostMapping(path="/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	@PostMapping(path = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<String> upload(
-		@RequestPart MultipartFile zipfile,
-		@RequestPart String name,
-		@RequestPart String title,
-		@RequestPart String comments
-	) {
-		logger.info(">>>>>springboot backend called");
+			@RequestPart MultipartFile zipfile,
+			@RequestPart String name,
+			@RequestPart String title,
+			@RequestPart String comments) throws FileNotFoundException{
 
-		//task 3 
+		// task 3
+		try {
+			
+			//add upload repo here
+
+			imgRepo.upload(zipfile);
+		}catch(IOException ex){
+			ex.printStackTrace();
+		}
+			
+			
+
+
+			// String stringZipFile = new String(zipfile.getBytes());
+
+			// byte[] buffer = new byte[1024];
+
+			// //!TODO: file not found exception
+			// ZipInputStream zis = new ZipInputStream(new FileInputStream(stringZipFile));
+
+			// ZipEntry zipEntry = zis.getNextEntry();
+
+			// while (zipEntry != null) {
+			// 	// extract files and send to s3
+			// 	File newFile = new File(zipEntry.getName());
+			// 	FileOutputStream fos = new FileOutputStream(newFile);
+			// 	int len;
+			// 	while ((len = zis.read(buffer)) > 0) {
+			// 		fos.write(buffer, 0, len);
+			// 	}
+			// 	fos.close();
+			// 	unzippedFiles.add(newFile);
+			// 	zipEntry = zis.getNextEntry();
+			// }
+
+			// for( File f: unzippedFiles){
+			// 	logger.info("files:", f.getName() );
+			// }
+			// zis.closeEntry();
+			// zis.close();
+
+		//upload to s3
 		
+		// } catch (IOException ex) {
+		// 	ex.printStackTrace();
+		// }
 		return null;
 	}
-	
 
 	// TODO: Task 5
-	
 
 	// TODO: Task 6
 
--- archive_backend/src/main/java/ibf2022/batch2/csf/backend/repositories/ImageRepository.java
+++ archive_backend/src/main/java/ibf2022/batch2/csf/backend/repositories/ImageRepository.java
@@ -1,11 +1,79 @@
 package ibf2022.batch2.csf.backend.repositories;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.CannedAccessControlList;
+import com.amazonaws.services.s3.model.ObjectMetadata;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+
+import net.lingala.zip4j.ZipFile;
+
+@Repository
 public class ImageRepository {
 
-	//TODO: Task 3
+	@Autowired
+	private AmazonS3 s3Client;
+
+	@Value("${DO_STORAGE_BUCKETNAME}")
+	private String bucketName;
+
+	// TODO: Task 3
 	// You are free to change the parameter and the return type
 	// Do not change the method's name
-	public Object upload(/* any number of parameters here */) {
-		return null;
+	public void upload(File file) throws IOException {
+
+		// ZipFile zipFile = new ZipFile(file);
+		// zipFile.extractAll("../.././Files");
+
+		System.out.println("enters image repo");
+		String fileString = file.getName();
+
+		ObjectMetadata metaData = new ObjectMetadata();
+
+		metaData.setContentType(bucketName);
+
+		Optional<String> fileExt = Optional.ofNullable(fileString)
+				.filter(f-> f.contains("."))
+				.map(f->f.substring(fileString.lastIndexOf("."), +1));
+		
+		metaData.setContentType("image/" + fileExt.get());
+
+		metaData.setContentLength(file.length());
+
+		PutObjectRequest putRequest = new PutObjectRequest(bucketName,fileString, new FileInputStream(file), metaData);
+		putRequest.withCannedAcl(CannedAccessControlList.PublicRead);
+		s3Client.putObject(putRequest);
+
 	}
+
+	//extraction of zip file
+	// public static final int BUFFER_SIZE = 4096;
+
+	// public void unzip(String zipFilePath, String destDirectory) throws IOException{
+	// 	File destDir = new File(destDirectory);
+	// 	if(!destDir.exists()){
+	// 		destDir.mkdir();
+	// 	}
+
+	// 	ZipInputStream zis = new ZipInputStream(new FileInputStream(zipFilePath));
+	// 	ZipEntry zipEntry = zis.getNextEntry();
+
+	// 	while(null!=zipEntry){
+	// 		String filePath = destDirectory + File.separator + zipEntry.getName();
+	// 		extractfile
+	// 	}
+	// }
+
 }
